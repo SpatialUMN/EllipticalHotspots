5a242e65a59e5812e975ff3c5fc8a7cc79edfea0
SpatialUMN <47234586+SpatialUMN@users.noreply.github.com>
2019-02-26 16:01:46 -0600

    Update RunElliptic.java

--- src/elliptical/RunElliptic.java
+++ src/elliptical/RunElliptic.java
@@ -3,9 +3,10 @@ import java.io.IOException;
 
 public class RunElliptic {
 	
-	static String dataset_path = "C:/Users/JingWe/Desktop/capstone/activity.csv";
+	static String dataset_path = "input/path/of/activity.csv";
 	static int Method = 0; //Naive = 0, Grid = 1
 	static int step_size =50; //only used when method = 0
+	static String writeFile = "path/to/naive/result.txt"; //naive result output path
 	static double theta = 730; // log likelihood ratio threshold only used when method = 1
 	
 	public static void main(String[] args) throws IOException {
